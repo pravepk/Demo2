b34608f4cd1b9f3fd23ceaeae6345f487f373b2e
pravepk <praveenapku@gmail.com>
2016-09-20 13:29:27 +0530

    Add files via upload

---
+++ TestCompleteRun.bat
@@ -0,0 +1,5 @@
+echo hello
+pause
+CALL "C:\Program Files (x86)\SmartBear\TestComplete 11\Bin\TestComplete.exe" "D:\User\Dileep\Project\TestComplete\TaleoWeb\TaleoWeb.pjs" /SilentMode /r /e
+echo called
+pause
